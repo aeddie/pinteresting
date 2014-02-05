8715ece04b07512a79146f722e48371eb960415f
Adam Hanneman <adam.hanneman@gmail.com>
2014-02-04 21:00:09 -0600

    Implemented devise for users

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass', "~> 3.1.0.0"
 
+gem "devise", "~> 3.2.2"
+
 group :development, :test do
 	gem 'sqlite3'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  # Required for Heroku (Set this to actual hose)
+  config.action_mailer.default_url_options = { :host => 'adam-pinteresting.herokuapp.com' }
 end
